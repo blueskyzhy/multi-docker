0e20020504e474b745298540b4009e522e1ad5c8
Tim Zheng <blueskyzhy@hotmail.com>
2021-01-27 20:46:53 +0800

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <div>
+          
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a><br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <Link to="/">Home</Link><br></br>
+          <Link to="/otherpage">Other Page</Link><br></br>
+        </div>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
